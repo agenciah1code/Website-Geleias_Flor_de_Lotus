59300f492af5947faaf8c88a6daa1ea436ef2f28
Agência H1 Code <contato@h1code.com.br>
2018-11-07 15:51:19 -0200

    Remoção do arquivo

--- loja-virtual.php
+++
@@ -1,5 +0,0 @@
-<?php include("includes/header.php") ?>
-
-<iframe width="100%" height="100%" frameborder="0" src="http://geleiasflordelotus.com.br/loja"></iframe>
-
-<?php include("includes/footer.php") ?>
